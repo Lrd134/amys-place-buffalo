507572dee330f20b1583c20a2a5844244cad7c73
Lrd134 <larryc3200@gmail.com>
2023-09-16 18:56:19 -0400

    updated navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,44 +1,74 @@
 import React from 'react';
 
 function NavBar() {
+  const handleMouseEnter = event => {
+    if (event.target.nodeName === "LI") {
+      event.target.children[0].children[0].className = ''
+    } else
+      event.target.children[0].className = '';
+
+  }
+  const handleMouseLeave = event => {
+    if (event.target.nodeName === "LI") {
+      event.target.children[0].children[0].className = 'hidden'
+    } else
+      event.target.children[0].className = 'hidden';
+
+  }
   return (
     <div className="navbar">
-      <a
-        className="navbar"
-        href="/"
-        alt="Home"
-      >
-        Home
-      </a>
-      <a
-        className="navbar"
-        href="/menu"
-        alt="Menu"
-      >
-        Menu
-      </a>
-      <a
-        className="navbar"
-        href="/events"
-        alt="Events"
-      >
-        Events
-      </a>
-      <a
-        className="navbar"
-        href="/gallery"
-        alt="Gallery"
-      >
-        Gallery
-      </a>      
-      <a
-        className="navbar"
-        href="/feedback"
-        alt="Feedback"
-      >
-        Feedback
-      </a>
-
+      <ul className="navbar">
+        <li className="navbar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <a
+            className="navbar"
+            href="/"
+            alt="Home"
+          >
+          <img className="hidden" src="cup_wh.png" alt="if-link-is-hovered-this-cup-shows"/>
+            Home
+          </a>
+        </li>
+        <li className="navbar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <a
+            className="navbar"
+            href="/menu"
+            alt="Menu"
+          >
+          <img className="hidden" src="cup_wh.png" alt="if-link-is-hovered-this-cup-shows"/>
+            Menu
+          </a>
+        </li>
+        <li className="navbar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <a
+            className="navbar"
+            href="/events"
+            alt="Events"
+          >
+          <img className="hidden" src="cup_wh.png" alt="if-link-is-hovered-this-cup-shows"/>
+            Events
+          </a>
+        </li>
+        <li className="navbar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <a
+            className="navbar"
+            href="/gallery"
+            alt="Gallery"
+          >
+          <img className="hidden" src="cup_wh.png" alt="if-link-is-hovered-this-cup-shows"/>
+            Gallery
+          </a>      
+        </li>
+        <li className="navbar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <a
+            className="navbar"
+            href="/feedback"
+            alt="Feedback"
+          >
+          <img className="hidden" src="cup_wh.png" alt="if-link-is-hovered-this-cup-shows"/>
+            Feedback
+          </a>
+        </li>
+    </ul>
     </div>
   )
 }
--- src/RootReducer.js
+++ src/RootReducer.js
@@ -1,6 +1,23 @@
-import { combineReducers } from 'redux';
-const RootReducer = combineReducers({
-
-})
-
+// import { combineReducers } from 'redux';
+const initialState = {
+  todos: [
+    { id: 0, text: 'Learn React', completed: true },
+    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
+  ],
+  filters: {
+    status: 'All',
+    colors: []
+  }
+}
+const RootReducer = function appReducer(state = initialState, action) {
+  // The reducer normally looks at the action type field to decide what happens
+  switch (action.type) {
+    // Do something here based on the different types of actions
+    default:
+      // If this reducer doesn't recognize the action type, or doesn't
+      // care about this specific action, return the existing state unchanged
+      return state
+  }
+}
 export default RootReducer
